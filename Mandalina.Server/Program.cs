3ec094f75305608dde2867ed2d01bba0c495dcda
Emir Cengiz <emircengiz21@gmail.com>
2022-12-15 17:30:53 +0300

    a

--- Mandalina.Server/Program.cs
+++ Mandalina.Server/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
+byte[] data = new byte[1024];
+
+IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+UdpClient newsock = new UdpClient(ipep);
+
+Console.WriteLine("Waiting for a client...");
+
+IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+data = newsock.Receive(ref sender);
+
+Console.WriteLine("Message received from {0}:", sender.ToString());
+Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
+
+string welcome = "Welcome to my test server";
+data = Encoding.ASCII.GetBytes(welcome);
+newsock.Send(data, data.Length, sender);
+
+while(true)
+{
+    data = newsock.Receive(ref sender);
+
+    Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
+    newsock.Send(data, data.Length, sender);
+}
